1cfdb497188029f97dfc312f22ef030b1f40a17e
TangThiHoa <hoarua592001@gmail.com>
2022-06-18 00:56:38 +0700

    xong hien thi theo giá ASC

--- src/main/java/com/example/demowebservice/controller/ProductController.java
+++ src/main/java/com/example/demowebservice/controller/ProductController.java
@@ -10,19 +10,22 @@ import com.example.demowebservice.service.IProductService;
 
 @Controller
 @CrossOrigin("*")  //cho phép truy cập từ dự án bên ngoài /Cấp quyền truy cập rộng hơn
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     IProductService productService;
-    @GetMapping("/products")
+    @GetMapping("")
     public ResponseEntity<Iterable<Product>> findAll(){
         return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
     }
-    @PostMapping("/products")
+    @PostMapping("")
     public ResponseEntity add(@RequestBody Product product){
         productService.sava(product);
         return new ResponseEntity(HttpStatus.OK);
-
-
+    }
+    @GetMapping("/order-by-price")
+    public ResponseEntity<Iterable<Product>> findAllByOrderByPrice(){
+        return new ResponseEntity<>(productService.findAllByOrderByPrice(), HttpStatus.OK);
     }
 
 }
